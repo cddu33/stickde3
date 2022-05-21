9ae7467886cdb1a80f01d037d6c609388285ddd1
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-05-21 15:28:23 +0200

    Update stickde3.class.php

--- core/class/stickde3.class.php
+++ core/class/stickde3.class.php
@@ -72,7 +72,7 @@ class stickde3Cmd extends cmd {
 		$var = str_replace('#', '', $var);
 		log::add('stickde3', 'debug', 'Après jeedom: ' . $var); 
         //$eqLogic->callstickde3($var);
-		return $var;
+		//return $var;
     }
   }
 }
