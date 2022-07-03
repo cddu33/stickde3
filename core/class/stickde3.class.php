1c1cb6fbf8b9fab8e0736c5ce5ea64975463e352
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-03 15:20:59 +0200

    $

--- core/class/stickde3.class.php
+++ core/class/stickde3.class.php
@@ -71,10 +71,11 @@ class stickde3 extends eqLogic {
 class stickde3Cmd extends cmd {
   public function execute($_options = array()) {
       $eqLogic = $this->getEqLogic();
-    switch ($this->getType()) {
-      case 'action' :
+    /*switch ($this->getType()) {
+      case 'action' :*/
         $eqLogic->callstickde3($this->getConfiguration('commande'));
         $eqlogic->checkAndUpdateCmd('last', '');
+       /* break;*/
     }
   }
 }
