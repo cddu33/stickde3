f222dc7d18633e8ba815987f3a3c575d365155c7
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-03 16:27:45 +0200

    s

--- core/class/stickde3.class.php
+++ core/class/stickde3.class.php
@@ -73,7 +73,7 @@ class stickde3Cmd extends cmd {
       case 'action' :*/
         $eqLogic->callstickde3($this->getConfiguration('commande'));
 
-        $eqLogic->setLogicalId('last', 'ff');
+        $eqLogic->setInfo('last', 'ff');
        /* break;
     }*/
   }
