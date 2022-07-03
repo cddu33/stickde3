28b9ebd93fe3c2749645ba0cea930f3f1b8e073d
cddu33 <59371705+cddu33@users.noreply.github.com>
2022-07-03 13:19:58 +0200

    Update stickde3.php

--- desktop/php/stickde3.php
+++ desktop/php/stickde3.php
@@ -8,59 +8,58 @@ sendVarToJS('eqType', $plugin->getId());
 $eqLogics = eqLogic::byType($plugin->getId());
 ?>
 
-<div class="row row-overflow">
-	<div class="col-xs-12 eqLogicThumbnailDisplay">
+  <div class="row row-overflow">
+<div class="col-xs-12 eqLogicThumbnailDisplay">
 		<legend><i class="fas fa-cog"></i>  {{Gestion}}</legend>
 		<!-- Boutons de gestion du plugin -->
 		<div class="eqLogicThumbnailContainer">
 			<div class="cursor eqLogicAction logoPrimary" data-action="add">
 				<i class="fas fa-plus-circle"></i>
 				<br>
-				<span>{{Ajouter}}</span>
-			</div>
-			<div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
-				<i class="fas fa-wrench"></i>
-				<br/>
-				<span>{{Configuration}}</span>
-			</div>
+        <span>{{Ajouter}}</span>
+      </div>
+      <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+        <i class="fas fa-wrench"></i>
+        <br/>
+        <span>{{Configuration}}</span>
+      </div>
 
-		</div>
+    </div>
 
-		<legend><i class="fas fa-home" id="butCol"></i> {{Mes Equipements}}</legend>
-		<?php
-			if (count($eqLogics) == 0) 
-			{
-				echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement Stickde3 trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
-			} else {
+    <legend><i class="fas fa-home" id="butCol"></i> {{Mes Equipements}}</legend>
+  <?php
+if (count($eqLogics) == 0) {
+			echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement Stickde3 trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
+		} else {
 			
-				echo '<div class="input-group" style="margin:5px;">';
-				echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic">';
-				echo '<div class="input-group-btn">';
-				echo '<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>';
-				echo '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
-				echo '</div>';
-				echo '</div>';
-				// Liste des équipements du plugin
-				echo '<div class="eqLogicThumbnailContainer">';
-				foreach ($eqLogics as $eqLogic) 
-				{
-					$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
-					echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
-					echo '<img src="' . $plugin->getPathImgIcon() . '">';
-					echo '<br>';
-					echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
-					echo '<span class="hiddenAsCard displayTableRight hidden">';
-					echo ($eqLogic->getIsVisible() == 1) ? '<i class="fas fa-eye" title="{{Equipement visible}}"></i>' : '<i class="fas fa-eye-slash" title="{{Equipement non visible}}"></i>';
-					echo '</span>';
-					echo '</div>';
-				}
+			echo '<div class="input-group" style="margin:5px;">';
+			echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic">';
+			echo '<div class="input-group-btn">';
+			echo '<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>';
+			echo '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
+			echo '</div>';
+			echo '</div>';
+			// Liste des équipements du plugin
+			echo '<div class="eqLogicThumbnailContainer">';
+			foreach ($eqLogics as $eqLogic) {
+				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+				echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+				echo '<img src="' . $plugin->getPathImgIcon() . '">';
+				echo '<br>';
+				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+				echo '<span class="hiddenAsCard displayTableRight hidden">';
+				echo ($eqLogic->getIsVisible() == 1) ? '<i class="fas fa-eye" title="{{Equipement visible}}"></i>' : '<i class="fas fa-eye-slash" title="{{Equipement non visible}}"></i>';
+				echo '</span>';
 				echo '</div>';
 			}
+			echo '</div>';
+		}
 		?>
 	</div> 
 
-	<div class="col-xs-12 eqLogic" style="display: none;">
-
+<div class="col-xs-12 eqLogic" style="display: none;">
+          
+		
 		<div class="input-group pull-right" style="display:inline-flex;">
 			<span class="input-group-btn">
 				<!-- Les balises <a></a> sont volontairement fermées à la ligne suivante pour éviter les espaces entre les boutons. Ne pas modifier -->
@@ -71,16 +70,18 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				</a>
 			</span>
 		</div>
-        
+		
+                  
 		<ul class="nav nav-tabs" role="tablist">
 			<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
 			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
 			<li role="presentation"><a href="#commandtab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-list"></i> {{Commandes}}</a></li>
 		</ul>
 		<div class="tab-content">
+		
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
 			
-				<form class="form-horizontal">
+	<form class="form-horizontal">
 					<fieldset>
 						<div class="col-lg-6">
 							<legend><i class="fas fa-wrench"></i> {{Paramètres généraux}}</legend>
@@ -97,12 +98,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
 									<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
 										<option value="">{{Aucun}}</option>
 										<?php
-											$options = '';
-											foreach ((jeeObject::buildTree(null, false)) as $object) 
-											{
-												$options .= '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
-											}
-											echo $options;
+										$options = '';
+										foreach ((jeeObject::buildTree(null, false)) as $object) {
+											$options .= '<option value="' . $object->getId() . '">' . str_repeat('&nbsp;&nbsp;', $object->getConfiguration('parentNumber')) . $object->getName() . '</option>';
+										}
+										echo $options;
 										?>
 									</select>
 								</div>
@@ -111,12 +111,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
 								<label class="col-sm-4 control-label">{{Catégorie}}</label>
 								<div class="col-sm-6">
 									<?php
-										foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) 
-										{
-											echo '<label class="checkbox-inline">';
-											echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" >' . $value['name'];
-											echo '</label>';
-										}
+									foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+										echo '<label class="checkbox-inline">';
+										echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" >' . $value['name'];
+										echo '</label>';
+									}
 									?>
 								</div>
 							</div>
@@ -130,27 +129,26 @@ $eqLogics = eqLogic::byType($plugin->getId());
 
 							<legend><i class="fas fa-cogs"></i> {{Paramètres spécifiques}}</legend>
 
-							<div class="form-group">
-								<label class="col-sm-3 control-label">{{Adresse IP de l'interface}}</label>
-								<div class="col-sm-3">
-									<input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="addr" placeholder="adresse IP de la carte relais"/>
-								</div>
-							</div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Adresse IP de l'interface}}</label>
+              <div class="col-sm-3">
+                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="addr" placeholder="adresse IP de la carte relais"/>
+              </div>
+            </div>
 
-							<div class="form-group">
-								<label class="col-sm-3 control-label">{{Port UDP de l'interface}}</label>
-								<div class="col-sm-3">
-									<input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="port" placeholder="Port de la carte relais"/>
-								</div>
-							</div>
-						</div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Port UDP de l'interface}}</label>
+              <div class="col-sm-3">
+                <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="port" placeholder="Port de la carte relais"/>
+              </div>
+            </div>
 
-					</fieldset>
-				</form>
-			</div>
+          </fieldset>
+        </form>
+      </div>
 
-			<div role="tabpanel" class="tab-pane" id="commandtab">
-				<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fas fa-plus-circle"></i> {{Commandes}}</a>
+    <div role="tabpanel" class="tab-pane" id="commandtab">
+			<a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fas fa-plus-circle"></i> {{Commandes}}</a>
 				<br><br>
 				<div class="table-responsive">
 					<table id="table_cmd" class="table table-bordered table-condensed">
@@ -158,7 +156,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 							<tr>
 								<th class="hidden-xs" style="min-width:50px;width:70px;">ID</th>
 								<th style="min-width:200px;width:350px;">{{Nom}}</th>
-								<th>{{Type}}</th>
+                                <th>{{Type}}</th>
 								<th style="min-width:260px;">{{Commande}}</th>
 								<th style="min-width:260px;">{{Options}}</th>
 								<th style="min-width:80px;width:200px;">{{Actions}}</th>
@@ -169,9 +167,8 @@ $eqLogics = eqLogic::byType($plugin->getId());
 					</table>
 				</div>
 			</div>
-		</div>
-	</div>
-</div>
+  </div>
+  </div>
 
 <?php include_file('desktop', 'stickde3', 'js', 'stickde3'); ?>
 <?php include_file('core', 'plugin.template', 'js'); ?>
